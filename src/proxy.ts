51f624dbf6431dda466db0b6dcd4752816c67e0c
Shivam <beastboyshiv5@gmail.com>
2026-06-10 13:41:04 +0530

    proxy(sessionId check) running before / route

---
+++ src/proxy.ts
@@ -0,0 +1,13 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+export default async function proxy(request: NextRequest) {
+    const sessionId = request.cookies.get("SessionId")?.value
+    if (!sessionId)
+        return NextResponse.redirect(new URL('/signin', request.url))
+
+    return NextResponse.next()
+}
+export const config = {
+    matcher: '/'
+}
+
