54a2eed4f2b3e16a494c48247ce7efa3db1ba7e2
Shivam <beastboyshiv5@gmail.com>
2026-06-12 17:09:00 +0530

    updated email validation for both empty strings and invalid email

--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -1,16 +1,16 @@
 import z from "zod"
 
 export const signUpSchema = z.object({
-    name: z.string(),
-    email: z.email({ message: "Invalid email address" }),
+    name: z.string().trim().min(1, "Name is required "),
+    email: z.email({ error: (issue) => issue.input === "" ? "Email is required" : "Invalid email address" }),
     password: z.string()
-        .trim()
-        .min(6, { message: "Password must contain at least 6 characters" })
-        .max(12, { message: "Password cannot contain more than 12 characters" })
+    .trim()
+    .min(6, { message: "Password must contain at least 6 characters" })
+    .max(12, { message: "Password cannot contain more than 12 characters" })
 })
 
 export const signInSchema = z.object({
-    email: z.email({ message: "Invalid email address" }),
+    email: z.email({ error: (issue) => issue.input === "" ? "Email is required" : "Invalid email address" }),
     password: z.string()
         .trim()
         .min(6, { message: "Password must contain at least 6 characters" })
