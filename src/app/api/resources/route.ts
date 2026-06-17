b6994b32f82e4005f8b362a549c2333f01e9e3ba
Shivam <beastboyshiv5@gmail.com>
2026-06-17 13:30:30 +0530

    add create tags functionality

--- src/app/api/resources/route.ts
+++ src/app/api/resources/route.ts
@@ -1,30 +1,41 @@
 import getCurrentUser from "@/lib/getUserId";
-import { createNote, createImage, createLink, createPdf } from "@/app/services/resources.service";
-
+import { createNote, createFile, createLink } from "@/app/services/resources.service";
+import { Prisma } from "@/generated/prisma/client";
 export async function POST(req: Request) {
-   const userId = await getCurrentUser();
-   if (!userId) return Response.json({ success: false, message: "UserId not found" }, { status: 401 })
-   const data = await req.json()
-   console.log(data)
-   const { title, description, tags, url, resourceType } = data.formData
-   let resourceId ;
-   switch (resourceType) {
-      case 'pdf':
+   try {
+      const userId = await getCurrentUser();
+      if (!userId) return Response.json({ success: false, message: "UserId not found" }, { status: 401 })
+      const data = await req.json()
+      const { title, description, tags, url, resourceType } = data.formData
+      let resourceId;
+      switch (resourceType) {
+         case 'pdf':
+            resourceId = await createFile(title, data.projectId, userId, resourceType, description, tags)
+            break;
+         case 'note':
+            resourceId = await createNote(title, data.projectId, userId, tags)
+            break;
+         case 'image':
+            resourceId = await createFile(title, data.projectId, userId, resourceType, description, tags)
+            break;
+         case 'link':
+            resourceId = await createLink(title, url, data.projectId, userId, description, tags)
+            break;
 
-         break;
-      case 'note':
-         createNote(title, data.projectId, userId)
-         break;
-      case 'image':
+         default:
+            break;
+      }
+      return Response.json({ success: true, message: 'resource created', id: resourceId!.id }, { status: 201 })
 
-         break;
-      case 'link':
-         createLink(title, url, data.projectId, userId, description)
-         break;
-
-      default:
-         break;
+   } catch (err: unknown) {
+      if (err instanceof Prisma.PrismaClientKnownRequestError && err.code === 'P2002') {
+         return Response.json({
+            success: false,
+            message: `Title already exists on the resource type `
+         }, { status: 409 })
+      }
+      return Response.json(
+         { success: false, err }, { status: 500 }
+      );
    }
-
-   return Response.json({ success: true, message: 'resource created' }, { status: 201 })
 }
